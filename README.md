b1cca8f7f92d8101091b5d825417f4f977b3a585
Milina Udara <milinaudara@live.com>
2014-05-24 17:16:09 +0530

    Data insertion

--- README.md
+++ README.md
@@ -14,18 +14,18 @@ The primary objective of the PasswordTracker app is to provide a learning app fo
 ###Quick Start
 Clone the repository:
 
-$ git clone https://github.com/milinaudara/PasswordTracker.git
+		$ git clone https://github.com/milinaudara/PasswordTracker.git
 
 Create database and user in pgAdmin and update the connection string ar server.js
 
 Install dependencies:
 
- $ cd PasswordTracker:
- $ npm install
+		$ cd PasswordTracker
+		$ npm install
 
 Start web server:
 
- $ npm start
+		$ npm start
 
 ###More Information
  *Blog Post [An Introduction to Sequelize.Js](http://milinaudara.wordpress.com/2014/05/24/an-introduction-to-sequelize-js/)
--- server.js
+++ server.js
@@ -5,13 +5,22 @@ var Sequelize = require('sequelize');
 // sequelize initialization
 var sequelize = new Sequelize("postgres://username:password@localhost:5432/dbname");
 
-// check database connection
-sequelize.authenticate().complete(function(err) {
-    if (err) {
-      console.log('Unable to connect to the database:', err);
-    } else {
-      console.log('Connection has been established successfully.');
-    }
+// model definition
+var User = sequelize.define("User", {
+    username: Sequelize.STRING,
+    password: Sequelize.STRING
+});
+
+//sync the model with the database
+sequelize.sync({ force: true }).success(function(err) {
+    // insert new user
+    User.create({
+        username: "john",
+        password: "a_secret"
+    }).success(function(user) {
+        // you can now access the newly created user via the variable user
+        console.log(user);
+    });
 });
 
 // initializing a port
